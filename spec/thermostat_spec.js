9851b808cdbec76dd37786ebe427ce5de248a174
Elishka Flint <elishkaf@gmail.com>
2018-07-16 13:20:37 +0100

    TEST Thermometer temperature can be increased and decreased

--- spec/thermostat_spec.js
+++ spec/thermostat_spec.js
@@ -7,7 +7,21 @@ describe('Thermostat:', function() {
   })
 
   it('starts at 20 degrees', function() {
-    expect(thermostat.getTemperature).toEqual(20)
+    expect(thermostat.getTemperature()).toEqual(20)
   })
-    
+
+  describe('.up', function() {
+    it('increases the temperature by one degree', function() {
+      thermostat.up()
+      expect(thermostat.getTemperature()).toEqual(21)
+    });
+  });
+
+  describe('.down', function() {
+    it('decreases the temperature by one degree', function() {
+      thermostat.down()
+      expect(thermostat.getTemperature()).toEqual(20)
+    });
+  });
+
 });
